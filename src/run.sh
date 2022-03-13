5274498c00169d61deba8c04dd0b8a2378716d2d
Matias Ezequiel Vara Larsen <matiasevara@gmail.com>
2022-03-13 17:01:50 +0000

    Update run.sh

--- src/run.sh
+++ src/run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 QEMU_PATH=~/qemufortoroos/build/x86_64-softmmu/qemu-system-x86_64
 mkdir -p floppy
-sudo mount -o loop floppy-disk.img ./floppy/
-sudo cp ../tools/sh/sh ./floppy/bin/sh
-sudo cp ../tools/ls/ls ./floppy/bin/ls
-sudo umount ./floppy
+mount -o loop floppy-disk.img ./floppy/
+cp ../tools/sh/sh ./floppy/bin/sh
+cp ../tools/ls/ls ./floppy/bin/ls
+umount ./floppy
 $QEMU_PATH -no-reboot -fda floppy-disk.img -kernel toro.elf -nographic -vnc :0 -m 32 -D qemu.log
