cf17d0ca4ed9f1127abea374962cf4514c11b2a8
Matias Ezequiel Vara Larsen <matiasevara@gmail.com>
2022-03-13 15:37:52 +0000

    Add simple script to run ToroOS on QEMU

---
+++ src/run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+QEMU_PATH=~/qemufortoroos/build/x86_64-softmmu/qemu-system-x86_64
+make
+mkdir -p floppy/bin
+sudo mount -o loop floppy-disk.img ./floppy/
+sudo cp ../tools/sh/sh ./floppy/bin/sh
+sudo cp ../tools/ls/ls ./floppy/bin/ls
+sudo umount ./floppy
+$QEMU_PATH -no-reboot -fda floppy-disk.img -kernel toro.elf -nographic -vnc :0 -m 32 -D qemu.log
